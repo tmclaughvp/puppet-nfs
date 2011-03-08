5f660f82bb434ebc35a81c59c807bafab23be2ed
shawnwebs <shawn@webs.com>
2011-03-08 09:33:59 -0500

    nfs-common not a service in ubuntu. portmap doesn't enable, it is called by other daemons

--- manifests/classes/nfs-client-debian.pp
+++ manifests/classes/nfs-client-debian.pp
@@ -5,17 +5,9 @@ class nfs::client::debian inherits nfs::base {
   }
   notice ('Including nfs::client::debian on $fqdn')
  
-  service { "nfs-common":
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-    require   => Package["nfs-common"],
-  }
- 
   service { "portmap":
     ensure    => running,
-    enable    => true,
-    hasstatus => false,
+    hasstatus => true,
     require   => Package["portmap"],
   }
 
