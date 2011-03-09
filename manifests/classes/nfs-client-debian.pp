b49a99f0ddd89be982c902ac940d12306e9ab108
shawnwebs <shawn@webs.com>
2011-03-09 16:02:54 -0500

    remove debugging

--- manifests/classes/nfs-client-debian.pp
+++ manifests/classes/nfs-client-debian.pp
@@ -3,7 +3,6 @@ class nfs::client::debian inherits nfs::base {
   package { ["nfs-common", "portmap"]:
     ensure => present,
   }
-  notice ('Including nfs::client::debian on $fqdn')
  
   service { "portmap":
     ensure    => running,
