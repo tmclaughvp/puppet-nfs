43a2d6f9bee538405ce3b2d08db492e2d550935f
shawnwebs <shawn@webs.com>
2011-08-10 09:14:38 -0400

    portmap should only listen on eth0

--- manifests/classes/nfs-client-debian.pp
+++ manifests/classes/nfs-client-debian.pp
@@ -3,6 +3,12 @@ class nfs::client::debian inherits nfs::base {
   package { ["nfs-common", "portmap"]:
     ensure => present,
   }
+
+  file { "/etc/default/portmap":
+    ensure => present, owner => root, group => root,
+    mode => 555, content => template("nfs/portmap.erb"),
+    require => Package["portmap"], notify => Service["portmap"],
+  }
  
   service { "portmap":
     ensure    => running,
